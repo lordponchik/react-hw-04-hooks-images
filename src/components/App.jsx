a845816cd57c034684aebd5e1e4aa990d1cada24
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-04-11 06:06:02 +0200

    remove class App and add func App with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
 import { requestImages } from 'services/api';
@@ -10,139 +10,216 @@ import Loader from './Loader/Loader';
 
 import s from './App.module.css';
 
-const DATA_IMAGES = {
-  images: [],
-  query: '',
-  currentPage: 1,
-  perPage: 12,
-  total: 0,
-};
+const PER_PAGE = 12;
 
-export class App extends Component {
-  state = {
-    ...DATA_IMAGES,
-    isLoading: false,
-    showLoadMore: false,
-    showModal: false,
-    modalData: null,
-  };
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(12);
+  const [total, setTotal] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showLoadMore, setShowLoadMore] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalData, setModalData] = useState(null);
 
-  componentDidUpdate(_, prevState) {
-    const { query, currentPage } = this.state;
+  useEffect(() => {
+    const sendingRequest = async () => {
+      if (query !== '') {
+        setIsLoading(true);
+        setShowLoadMore(true);
 
-    if (
-      (query !== prevState.query && query !== '') ||
-      (currentPage !== prevState.currentPage && query !== '')
-    ) {
-      this.sendingRequest();
-    }
-  }
+        const restImages = total - perPage * currentPage;
 
-  sendingRequest = async () => {
-    const { query, currentPage, total, perPage } = this.state;
+        if (total !== 0 && restImages < perPage) {
+          setPerPage(restImages);
+        }
 
-    this.setState({
-      isLoading: true,
-      showLoadMore: false,
-    });
+        try {
+          const { hits, totalHits } = await requestImages(
+            query,
+            currentPage,
+            perPage
+          );
 
-    const restImages = total - perPage * currentPage;
+          if (hits.length === 0) {
+            toast.warn('Nothing found');
+          }
 
-    if (total !== 0 && restImages < perPage) {
-      this.setState({ perPage: restImages });
-    }
-
-    try {
-      const { hits, totalHits } = await requestImages(
-        query,
-        currentPage,
-        perPage
-      );
-
-      if (hits.length === 0) {
-        toast.warn('Nothing found');
+          setImages(prevImages => [...prevImages, ...hits]);
+          setTotal(totalHits);
+          setShowLoadMore(currentPage < Math.ceil(totalHits / PER_PAGE));
+        } catch (error) {
+          toast.warn(error.message);
+        } finally {
+          setIsLoading(false);
+        }
       }
+    };
 
-      this.setState(prevState => {
-        return {
-          images: [...prevState.images, ...hits],
-          total: totalHits,
-          showLoadMore:
-            currentPage < Math.ceil(totalHits / DATA_IMAGES.perPage),
-        };
-      });
-    } catch (error) {
-      toast.warn(error.message);
-    } finally {
-      this.setState({
-        isLoading: false,
-      });
-    }
-  };
+    sendingRequest();
+  }, [query, currentPage]);
 
-  receivingRequest = q => {
+  const receivingRequest = q => {
     if (q === null) {
-      this.setState({
-        ...DATA_IMAGES,
-        showLoadMore: false,
-      });
+      setImages([]);
+      setQuery('');
+      setCurrentPage(1);
+      setPerPage(PER_PAGE);
+      setTotal(0);
+      setShowLoadMore(false);
       return;
     }
 
-    if (this.state.query === q) {
+    if (query === q) {
       toast.warn(`"${q}" found`);
       return;
     }
 
-    this.setState({
-      ...DATA_IMAGES,
-      query: q,
-    });
+    setImages([]);
+    setQuery(q);
+    setCurrentPage(1);
+    setPerPage(PER_PAGE);
+    setTotal(0);
   };
 
-  nextPage = () => {
-    this.setState(prevState => {
-      return { currentPage: prevState.currentPage + 1 };
-    });
+  const nextPage = () => {
+    setCurrentPage(perCurrentPage => perCurrentPage + 1);
   };
 
-  toggleModal = data => {
-    this.setState(prevState => {
-      return {
-        showModal: !prevState.showModal,
-        modalData: prevState.modalData === null ? { ...data } : null,
-      };
-    });
+  const toggleModal = data => {
+    setShowModal(prevShowModal => !prevShowModal);
+    setModalData(prevModalData =>
+      prevModalData === null ? { ...data } : null
+    );
+
     document.body.classList.toggle('no-scroll');
   };
 
-  render() {
-    const {
-      images,
-      showLoadMore,
-      isLoading,
-      showModal,
-      modalData,
-      currentPage,
-    } = this.state;
-
-    return (
-      <div className={s.app}>
-        <ToastContainer></ToastContainer>
-        <Searchbar receivingRequest={this.receivingRequest}></Searchbar>
-        {images.length > 0 && (
-          <ImageGallery
-            images={images}
-            currentPage={currentPage}
-            openModal={this.toggleModal}
-          ></ImageGallery>
-        )}
-        {isLoading && <Loader></Loader>}
-        {showLoadMore && <LoadMore nextPage={this.nextPage}></LoadMore>}
-        {showModal && (
-          <Modal data={modalData} closeModal={this.toggleModal}></Modal>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className={s.app}>
+      <ToastContainer></ToastContainer>
+      <Searchbar receivingRequest={receivingRequest}></Searchbar>
+      {images.length > 0 && (
+        <ImageGallery
+          images={images}
+          currentPage={currentPage}
+          openModal={toggleModal}
+        ></ImageGallery>
+      )}
+      {isLoading && <Loader></Loader>}
+      {showLoadMore && <LoadMore nextPage={nextPage}></LoadMore>}
+      {showModal && <Modal data={modalData} closeModal={toggleModal}></Modal>}
+    </div>
+  );
 }
+
+// export class App extends Component {
+// state = {
+//   ...DATA_IMAGES,
+//   isLoading: false,
+//   showLoadMore: false,
+//   showModal: false,
+//   modalData: null,
+// };
+
+// componentDidUpdate(_, prevState) {
+//   const { query, currentPage } = this.state;
+
+//   if (
+//     (query !== prevState.query && query !== '') ||
+//     (currentPage !== prevState.currentPage && query !== '')
+//   ) {
+//     this.sendingRequest();
+//   }
+// }
+
+// sendingRequest = async () => {
+//   const { query, currentPage, total, perPage } = this.state;
+
+//   this.setState({
+//     isLoading: true,
+//     showLoadMore: false,
+//   });
+
+//   const restImages = total - perPage * currentPage;
+
+//   if (total !== 0 && restImages < perPage) {
+//     this.setState({ perPage: restImages });
+//   }
+
+//   try {
+//     const { hits, totalHits } = await requestImages(
+//       query,
+//       currentPage,
+//       perPage
+//     );
+
+//     if (hits.length === 0) {
+//       toast.warn('Nothing found');
+//     }
+
+//     this.setState(prevState => {
+//       return {
+//         images: [...prevState.images, ...hits],
+//         total: totalHits,
+//         showLoadMore:
+//           currentPage < Math.ceil(totalHits / DATA_IMAGES.perPage),
+//       };
+//     });
+//   } catch (error) {
+//     toast.warn(error.message);
+//   } finally {
+//     this.setState({
+//       isLoading: false,
+//     });
+//   }
+// };
+
+// receivingRequest = q => {
+//   if (q === null) {
+//     this.setState({
+//       ...DATA_IMAGES,
+//       showLoadMore: false,
+//     });
+//     return;
+//   }
+
+//   if (this.state.query === q) {
+//     toast.warn(`"${q}" found`);
+//     return;
+//   }
+
+//   this.setState({
+//     ...DATA_IMAGES,
+//     query: q,
+//   });
+// };
+
+//   nextPage = () => {
+//     this.setState(prevState => {
+//       return { currentPage: prevState.currentPage + 1 };
+//     });
+//   };
+
+//   toggleModal = data => {
+//     this.setState(prevState => {
+//       return {
+//         showModal: !prevState.showModal,
+//         modalData: prevState.modalData === null ? { ...data } : null,
+//       };
+//     });
+//     document.body.classList.toggle('no-scroll');
+//   };
+
+//   render() {
+//     const {
+//       images,
+//       showLoadMore,
+//       isLoading,
+//       showModal,
+//       modalData,
+//       currentPage,
+//     } = this.state;
+//   }
+// }
